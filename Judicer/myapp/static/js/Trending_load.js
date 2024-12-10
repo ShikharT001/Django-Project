7bef8c02b18807cbd464f5a16e2a6d1953f23c9d
Macner001 <104127090+Macner001@users.noreply.github.com>
2024-12-10 12:41:39 +0530

    new file:   Judicer/myapp/static/Images/favicon_io/about.txt 	new file:   Judicer/myapp/static/Images/favicon_io/android-chrome-192x192.png 	new file:   Judicer/myapp/static/Images/favicon_io/android-chrome-512x512.png 	new file:   Judicer/myapp/static/Images/favicon_io/apple-touch-icon.png 	new file:   Judicer/myapp/static/Images/favicon_io/favicon-16x16.png 	new file:   Judicer/myapp/static/Images/favicon_io/favicon-32x32.png 	new file:   Judicer/myapp/static/Images/favicon_io/favicon.ico 	new file:   Judicer/myapp/static/Images/favicon_io/site.webmanifest 	modified:   Judicer/myapp/static/css/home.css 	modified:   Judicer/myapp/static/css/index.css 	modified:   Judicer/myapp/static/js/Trending_load.js 	modified:   Judicer/myapp/templates/home.html 	modified:   Judicer/myapp/templates/layout/header.html 	modified:   Judicer/myapp/templates/main.html

--- Judicer/myapp/static/js/Trending_load.js
+++ Judicer/myapp/static/js/Trending_load.js
@@ -24,7 +24,7 @@ document.addEventListener('DOMContentLoaded', function () {
                       <p>${item.Release_date}</p>
                        </div> 
                        </div> `;
-                       
+              
 
         })
         
